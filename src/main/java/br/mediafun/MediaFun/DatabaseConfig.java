24f33f3e0fb946c6ebd97ff168d1930cf1382e48
hyigor <48020648+hyigor@users.noreply.github.com>
2021-05-25 00:17:03 -0300

    b

--- src/main/java/br/mediafun/MediaFun/DatabaseConfig.java
+++ src/main/java/br/mediafun/MediaFun/DatabaseConfig.java
@@ -3,6 +3,8 @@ package br.mediafun.MediaFun;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,13 +14,33 @@ import javax.sql.DataSource;
 public class DatabaseConfig {
 
 
-        @Value("${spring.datasource.url}")
-        private String dbUrl;
+//        @Value("${spring.datasource.url}")
+//        private String dbUrl;
+//
+//        @Bean
+//        public DataSource dataSource() {
+//            HikariConfig config = new HikariConfig();
+//            config.setJdbcUrl(dbUrl);
+//            return new HikariDataSource(config);
+//        }
 
-        @Bean
-        public DataSource dataSource() {
-            HikariConfig config = new HikariConfig();
-            config.setJdbcUrl(dbUrl);
-            return new HikariDataSource(config);
-        }
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClass;
+    @Value("${spring.datasource.data-username}")
+    private String url;
+    @Value("${spring.datasource.data-username}")
+    private String userName;
+    @Value("${spring.datasource.data-password}")
+    private String password;
+
+    @Bean
+    public DataSource getDataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName(driverClass)
+                .url(url)
+                .username(userName)
+                .password(password)
+                .build();
+    }
 }
