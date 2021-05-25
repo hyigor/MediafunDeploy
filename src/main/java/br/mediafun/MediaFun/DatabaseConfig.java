d5bfcc720537eac74529b525af2feeead82909a3
hyigor <48020648+hyigor@users.noreply.github.com>
2021-05-25 00:20:37 -0300

    c

--- src/main/java/br/mediafun/MediaFun/DatabaseConfig.java
+++ src/main/java/br/mediafun/MediaFun/DatabaseConfig.java
@@ -25,22 +25,22 @@ public class DatabaseConfig {
 //        }
 
 
-    @Value("${spring.datasource.driver-class-name}")
-    private String driverClass;
-    @Value("${spring.datasource.data-username}")
-    private String url;
-    @Value("${spring.datasource.data-username}")
-    private String userName;
-    @Value("${spring.datasource.data-password}")
-    private String password;
+//    @Value("${spring.datasource.driver-class-name}")
+//    private String driverClass;
+//    @Value("${spring.datasource.data-username}")
+//    private String url;
+//    @Value("${spring.datasource.data-username}")
+//    private String userName;
+//    @Value("${spring.datasource.data-password}")
+//    private String password;
 
     @Bean
     public DataSource getDataSource() {
         return DataSourceBuilder.create()
-                .driverClassName(driverClass)
-                .url(url)
-                .username(userName)
-                .password(password)
+                .driverClassName("org.postgresql.Driver")
+                .url("postgres://bjakctqhtmqclj:8e31264cc64bf1d08c4fffd46a3aa68e0ddb08b328d8b23fdfc89befce12ee0f@ec2-54-160-96-70.compute-1.amazonaws.com:5432/dbrr490nasf5gh")
+                .username("bjakctqhtmqclj")
+                .password("8e31264cc64bf1d08c4fffd46a3aa68e0ddb08b328d8b23fdfc89befce12ee0f")
                 .build();
     }
 }
